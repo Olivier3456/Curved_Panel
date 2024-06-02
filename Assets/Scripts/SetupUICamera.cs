7ad220df545a8d710cca60ac0a98cee8a28d57db
Olivier3456 <olivier.larrey@gmail.com>
2024-06-02 17:45:30 +0200

    Render Texture material now created in script + shader Unlit/Transparent added to "Always Included Shaders" list (Graphics Settings)

--- Assets/Scripts/SetupUICamera.cs
+++ Assets/Scripts/SetupUICamera.cs
@@ -11,17 +11,14 @@ public class SetupUICamera : MonoBehaviour
     public RectTransform panelRectTransform;
     public Renderer screenToDisplayPanel;
     public int pixelsPerMeter = 256;
-    [Space(20)]
-    public Material debug_Mat;
-    //public RenderTexture debug_RenderTexture;
-
 
 
     private void Start()
     {
         CreateRenderTexture();
-        //CreateMaterial();
-        ApplyMaterialToObject();
+        mat = new Material(Shader.Find("Unlit/Transparent"));   // DON'T FORGET TO INCLUDE THIS SHADER IN "ALWAYS INCLUDED SHADERS" LIST
+        mat.mainTexture = rd;
+        screenToDisplayPanel.material = mat;
         SetupCamera(panelRectTransform);
     }
 
@@ -33,53 +30,9 @@ public class SetupUICamera : MonoBehaviour
 
         rd = new RenderTexture((int)width, (int)height, 24);
 
-        debug_Mat.mainTexture = rd;
-
         Debug.Log($"Render texture created. Width = {(int)width}, height = {(int)height}.");
     }
-
-
-    //private void SetRenderTextureSize()   // We can't adjust a Render texture size after its creation...
-    //{
-    //    float width = panelRectTransform.rect.width * pixelsPerMeter;
-    //    float height = panelRectTransform.rect.height * pixelsPerMeter;
-
-    //    debug_RenderTexture.width = (int)width;
-    //    debug_RenderTexture.height = (int)height;               
-
-    //    Debug.Log($"Render texture size changed. Width = {(int)width}, height = {(int)height}.");
-    //}
-
-
-
-    //private void CreateMaterial()
-    //{
-    //    mat = new Material(Shader.Find("Standard"));
-    //    //ToFadeMode();
-    //    mat.mainTexture = rd;
-    //}
-
-
-    //private void ToFadeMode()
-    //{
-    //    mat.SetOverrideTag("RenderType", "Transparent");
-    //    mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-    //    mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-    //    mat.SetInt("_ZWrite", 0);
-    //    mat.DisableKeyword("_ALPHATEST_ON");
-    //    mat.EnableKeyword("_ALPHABLEND_ON");
-    //    mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-    //    mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-    //}
-
-
-    private void ApplyMaterialToObject()
-    {
-        //screenToDisplayPanel.material = mat;
-        screenToDisplayPanel.material = debug_Mat;
-    }
-
-
+    
     public void SetupCamera(RectTransform rectTransform)
     {
         cam = gameObject.AddComponent<Camera>();
